9db75b678ce2f2e8066ef3aec2acdcc37a9c0ed0
Kristjan Mändmets <kristjan.mandmets@ppe.ee>
2022-04-28 21:28:30 +0300

    add new projects

---
+++ GuessingGameInfinite/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace GuessingGameInfinite
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			//programm genereerib juhuslikku numbrit 1 kuni 10;
+			//kasutaja peab numbri ära arvama;
+			//kui kasutaja suutis numbri arvata, siis ta on mängu võitnud;
+			//katsete arv on piiramatu;
+			//*programm genereerib juhuslikku numbrit ühe korra.
+
+			Random rnd = new Random();
+			int cpuNumber = rnd.Next(1, 11);
+			bool loopActive = true;
+			int i = 0;
+
+
+			while (loopActive)
+			{
+				Console.WriteLine("Oled valmis?");
+				Console.WriteLine("Arva ära, mis number jääb vahemikku 1 kuni 10?");
+				int userNumber = Convert.ToInt32(Console.ReadLine());
+
+				if (userNumber == cpuNumber)
+				{
+					Console.WriteLine("Palju õnne! Oled võitnud.");
+					loopActive = false;
+				}
+				else
+				{
+					i++;
+					Console.WriteLine("Vale number, proovi uuesti!");
+					Console.WriteLine($"Oled sisestanud {i} korda vale numbri.");
+				}
+				Console.WriteLine("Nägemist!");
+			}
+		}
+	}
+}
---
+++ GuessingGameThreeTries/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace GuessingGameThreeTries
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			//programm genereerib juhuslikku numbrit 1 kuni 10;
+			//kasutaja peab numbri ära arvama;
+			//kui kasutaja suutis numbri arvata, siis ta on mängu võitnud;
+			//katsete arv on 3, kui ei ole suutnud arvata, siis mängu võidab arvuti;
+			//*programm genereerib juhuslikku numbrit ühe korra.
+
+			Random rnd = new Random();
+			int cpuNumber = rnd.Next(1, 11);
+			int i = 0;
+
+
+			while (i < 3)
+			{
+				Console.WriteLine("Oled valmis?");
+				Console.WriteLine("Arva ära, mis number jääb vahemikku 1 kuni 10?");
+				int userNumber = Convert.ToInt32(Console.ReadLine());
+
+				if (userNumber == cpuNumber)
+				{
+					Console.WriteLine("Palju õnne! Oled võitnud.");
+					break;
+				}
+				else
+				{
+					i++;
+					Console.WriteLine("Vale number!");
+					Console.WriteLine($"Oled sisestanud {i} korda vale numbri. Jäänud on {3 - i} katset.");
+				}
+				Console.WriteLine("Nägemist!");
+			}
+		}
+	}
+}
